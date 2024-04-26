eca05e68fd793b3dc758154c2d16d73c66a67874
Ilia <isinetov@mail.ru>
2024-04-26 20:00:11 +0400

    Задание 7.1.4 Для следующего класса Employee создайте 2 наследника: ProjectManager и Developer. Класс ProjectManager должен содержать строковое поле ProjectName, а класс Developer — строковое поле ProgrammingLanguage.

--- Task 7_1_4/Task 7_1_4.cs
+++ Task 7_1_4/Task 7_1_4.cs
@@ -1,1 +1,16 @@
-﻿
+﻿class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+
+class ProjectManager : Employee 
+{
+    public string ProjectName;
+}
+
+class Developer : Employee 
+{
+    public string ProgrammingLanguage;
+}
