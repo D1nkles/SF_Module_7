a18915b90848d681b4dfcc4ca9a66d2f448c14e4
Ilia <isinetov@mail.ru>
2024-04-26 22:46:28 +0400

    Задание 7.2.12. Для класса Obj перегрузите операторы + и -, чтобы результатом работы оператора был новый экземпляр класса Obj, а операции производились над полем Value.

---
+++ Task 7_2_12/Program.cs
@@ -0,0 +1,34 @@
+﻿class Obj
+{
+    public int Value;
+
+    public static Obj operator +(Obj a, Obj b) 
+    {
+        return new Obj
+        {
+            Value = a.Value + b.Value
+        };
+    }
+
+    public static Obj operator -(Obj a, Obj b) 
+    {
+        return new Obj
+        {
+            Value = a.Value - b.Value
+        };
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        var obj1 = new Obj() { Value = 15 };
+        var obj2 = new Obj() { Value = 7 };
+        
+        Obj obj3 = obj1 + obj2;
+        Obj obj4 = obj1 - obj2;
+
+        Console.WriteLine(obj3.Value + " " + obj4.Value);
+    }
+}
