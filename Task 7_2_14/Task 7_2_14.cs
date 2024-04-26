39d2cfd680734e6790a7cd220d114242690db07c
Ilia <isinetov@mail.ru>
2024-04-26 23:08:07 +0400

    Задание 7.2.14. Для следующего класса напишите индексатор, для типа параметра используйте int

---
+++ Task 7_2_14/Task 7_2_14.cs
@@ -0,0 +1,46 @@
+﻿class IndexingClass
+{
+    private int[] array;
+
+    public IndexingClass(int[] array)
+    {
+        this.array = array;
+    }
+
+    public int this[int index] 
+    {
+        get 
+        {
+            if (index >= 0 && index < array.Length) 
+            {
+                return array[index];
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+
+        set 
+        {
+            if (index >= 0 && index < array.Length) 
+            {
+                array[index] = value;
+            }
+        }
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        int[] arr = { 52, 21, 97, 165, 2, 6 };
+
+        var indexing = new IndexingClass(arr);
+        int num1 = indexing[2];
+        indexing[3] = 555;
+        int num2 = indexing[3];
+        Console.WriteLine($"{num1} {num2}");
+    }
+}
