9f861de523ca05aae0f0eda0e604e887cfd5a0d9
Ilia <isinetov@mail.ru>
2024-04-26 23:23:14 +0400

    Задание 7.3.3. Создайте классы для следующих объектов компьютера: процессор (Processor), материнская карта (MotherBoard), видеокарта (GraphicCard). Унаследуйте их от класса ComputerPart.
    Добавьте в класс ComputerPart абстрактный метод Work без параметров и с типом void.

---
+++ Task 7_3_3/Task 7_3_3.cs
@@ -0,0 +1,28 @@
+﻿abstract class ComputerPart
+{
+    public abstract void Work();
+}
+
+class Processor : ComputerPart 
+{
+    public override void Work()
+    {
+        Console.WriteLine("Процессор работает!");
+    }
+}
+
+class MotherBoard : ComputerPart 
+{
+    public override void Work() 
+    {
+        Console.WriteLine("Материнская плата работает!");
+    }
+}
+
+class GraphicCard : ComputerPart 
+{
+    public override void Work() 
+    {
+        Console.WriteLine("Видеокарта работает!");
+    }
+}
