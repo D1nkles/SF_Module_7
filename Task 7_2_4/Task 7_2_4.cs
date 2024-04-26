74aaad13594117c0cb1451a061aa516a7b4d0294
Ilia <isinetov@mail.ru>
2024-04-26 21:00:21 +0400

    Задание 7.2.4 Измените свойство Counter так, чтобы его можно было переопределить в классе DerivedClass. Переопределите данное свойство, ограничив занесения в него чисел меньше 0.

---
+++ Task 7_2_4/Task 7_2_4.cs
@@ -0,0 +1,30 @@
+﻿class BaseClass
+{
+    public virtual int Counter
+    {
+        get;
+        set;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public override int Counter 
+    {
+        get 
+        {
+            return Counter;
+        }
+        set 
+        {
+            if (value < 0) 
+            {
+                Console.WriteLine("Число меньше 0!");
+            }
+            else 
+            {
+                Counter = value;
+            }
+        }
+    }
+}
