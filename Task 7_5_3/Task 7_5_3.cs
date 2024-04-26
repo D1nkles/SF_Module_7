212dec1cb161bf18adf123ef9dce674c1a4c2744
Ilia <isinetov@mail.ru>
2024-04-27 00:01:46 +0400

    Задание 7.5.3. Создайте класс Helper и определите в нем статический метод Swap типа void, который принимает 2 переменные типа int и меняет их значения местами.

---
+++ Task 7_5_3/Task 7_5_3.cs
@@ -0,0 +1,28 @@
+﻿class Helper
+{
+    public static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public static void Swap(ref int num1, ref int num2) 
+    {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        int num1 = 3;
+        int num2 = 58;
+
+        Helper.Swap(ref num1, ref num2);
+
+        Console.WriteLine(num1); //58
+        Console.WriteLine(num2); //3
+    }
+}
