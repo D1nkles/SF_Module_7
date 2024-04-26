756473366188e0e21b6e300edde602140c96afa8
Ilia <isinetov@mail.ru>
2024-04-27 00:18:02 +0400

    Задание 7.5.5. Измените класс Obj так, чтобы статические поля инициализировались в статическом конструкторе

---
+++ Task 7_5_5/Task 7_5_5.cs
@@ -0,0 +1,25 @@
+﻿class Obj
+{
+    public string Name;
+    public string Description;
+
+    public static string Parent;
+    public static int DaysInWeek;
+    public static int MaxValue;
+
+    static Obj() 
+    {
+        Parent = "System.Object";
+        DaysInWeek = 7;
+        MaxValue = 2000;
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        var obj = new Obj();
+        Console.WriteLine(Obj.Parent + " " + Obj.DaysInWeek + " " + Obj.MaxValue);
+    }
+}
