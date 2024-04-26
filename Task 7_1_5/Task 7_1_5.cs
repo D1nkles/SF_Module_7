cb064239b8321995165d35b4c13b80deb8a4ad19
Ilia <isinetov@mail.ru>
2024-04-26 20:05:41 +0400

    Задание 7.1.5 Для следующего списка объектов создайте схему классов (объявите нужные классы и установите связи между ними):
    Apple (яблоко);
    Banana (банан);
    Pear (груша);
    Potato (картофель);
    Carrot (морковь).

---
+++ Task 7_1_5/Task 7_1_5.cs
@@ -0,0 +1,8 @@
+﻿class Food { }
+class Vegetable : Food { }
+class Fruit : Food { }
+class Apple : Fruit { }
+class Banana : Fruit { }
+class Pear : Fruit { }
+class Potato : Vegetable { }
+class Carrot : Vegetable { }
